34f046f5bfe34d1f2da0a28d41eed2c4b4147471
shyzik93 <shyzik93@mail.ru>
2025-03-28 21:56:40 +0300

    add draft

--- src/api_adapters/tg_adapter/src.py
+++ src/api_adapters/tg_adapter/src.py
@@ -1,7 +1,26 @@
+import json
+
+import requests
 from kafka import KafkaConsumer
 
-print('Hello, world')
+token = ''
+url = f'https://api.telegram.org/bot{token}'
+
+
+def get_chat_info(channel_id):
+    channel = {'channel_id': channel_id}
+    params = {'channel': channel}
+    response = requests.post(f'{url}/getChat', json=params)
+    print(response.status_code, response.content)
+
+
+def link2channel_id(link):
+    pass
+
 
 consumer = KafkaConsumer('my_favorite_topic')
-for msg in consumer:
-    print(msg)
+for message in consumer:
+    data = json.loads(message)
+    channel_id = link2channel_id(data['link'])
+    get_chat_info(channel_id)
+    print(message)
