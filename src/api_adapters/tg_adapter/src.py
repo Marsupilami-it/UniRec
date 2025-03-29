9a499dcf34351a041fa4b18dd8e44babd6789d51
shyzik93 <shyzik93@mail.ru>
2025-03-29 09:39:55 +0300

    getting groups info is working

--- src/api_adapters/tg_adapter/src.py
+++ src/api_adapters/tg_adapter/src.py
@@ -1,26 +1,43 @@
 import json
+import os
 
 import requests
+from dotenv import load_dotenv
 from kafka import KafkaConsumer
 
-token = ''
-url = f'https://api.telegram.org/bot{token}'
+load_dotenv()
+
+TOKEN = os.getenv('TG_TOKEN')
+URL = f'https://api.telegram.org/bot{TOKEN}'
 
 
 def get_chat_info(channel_id):
-    channel = {'channel_id': channel_id}
-    params = {'channel': channel}
-    response = requests.post(f'{url}/getChat', json=params)
-    print(response.status_code, response.content)
+    params = {'chat_id': channel_id}
+    response = requests.post(f'{URL}/getChat', json=params)
+    if response.status_code == 200:
+        response_json = response.json()
+        if response_json.get('ok'):
+            result = response_json['result']
+            pinned_message = result.get('pinned_message', {}).get('text', '')
+            return {
+                'title': result['title'],
+                'short_description': result['description'],
+                'description': pinned_message,
+            }
 
 
 def link2channel_id(link):
-    pass
+    if link.startswith('@'):
+        return link
+    
+    if link.startswith('https://t.me/') or link.startswith('t.me/'):
+        chat_name = link.split('/')[-1]
+        return f'@{chat_name}'
 
 
 consumer = KafkaConsumer('my_favorite_topic')
 for message in consumer:
     data = json.loads(message)
     channel_id = link2channel_id(data['link'])
-    get_chat_info(channel_id)
-    print(message)
+    data = get_chat_info(channel_id)
+    print(message, data)
