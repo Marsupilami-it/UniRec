8911e15decf8305458fa946c114d4aec49209472
shyzik93 <shyzik93@mail.ru>
2025-03-30 13:53:40 +0300

    add server to llm

---
+++ src/ml_pipeline/server.py
@@ -0,0 +1,53 @@
+import json
+
+import pika
+from worker import find_societies_by_link
+
+
+channel = None
+
+def callback(ch, method, properties, body):
+    print(body)
+    recieved_data = json.loads(body)
+    answer = find_societies_by_link(recieved_data['link'])
+    print(answer)
+    print()
+    data = {'description': answer}
+    data['connection_id'] = recieved_data['connection_id']
+    channel.basic_publish(exchange='', routing_key='api-adapters-output', body=json.dumps(data))
+
+
+def on_connected(connection):
+    """Called when we are fully connected to RabbitMQ"""
+    connection.channel(on_open_callback=on_channel_open)
+    print('connected', connection)
+
+
+def on_close(connection, exception):
+    # Invoked when the connection is closed
+    connection.ioloop.stop()
+    print('stopped', exception)
+
+# Step #3
+def on_channel_open(new_channel):
+    """Called when our channel has opened"""
+    global channel
+    channel = new_channel
+    # channel.queue_declare(queue="test", durable=True, exclusive=False, auto_delete=False, callback=on_queue_declared)
+    channel.basic_consume(queue='api-adapter-tg', auto_ack=True, on_message_callback=callback)
+    channel.queue_declare(queue='api-adapter-tg')
+    channel.queue_declare(queue='api-adapters-output')
+    print('channel openned', channel)
+
+
+connection = pika.SelectConnection(pika.ConnectionParameters('localhost'), on_open_callback=on_connected, on_close_callback=on_close)
+
+try:
+    # Loop so we can communicate with RabbitMQ
+    connection.ioloop.start()
+except KeyboardInterrupt:
+    # Gracefully close the connection
+    connection.close()
+    # Loop until we're fully closed.
+    # The on_close callback is required to stop the io loop
+    connection.ioloop.start()
--- src/ml_pipeline/worker.py
+++ src/ml_pipeline/worker.py
@@ -5,21 +5,21 @@ from llm_client import LLMClient
 from llm_handler import LLMHandler
 
 
-def main():
+def find_societies_by_link(link):
     model_name = os.getenv('MODEL_NAME', 'Maoyue/c4ai-command-r7b-12-2024')
     llm_client = LLMClient(model_name)
     llm_handler = LLMHandler(llm_client)
 
+    prompt = "Исходя из названия самой ссылки, найди аналогичные сообщества и перечисли их, обращай внимание именно на конец ссылки."
+    return llm_handler.process_link(link, prompt)
+
+
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         link = sys.argv[1]
     else:
         link = "https://vk.com/fincultinfo"
 
-    prompt = "Исходя из названия самой ссылки, найди аналогичные сообщества и перечисли их, обращай внимание именно на конец ссылки."
-    answer = llm_handler.process_link(link, prompt)
-    if answer:
-        print(answer)
-
-
-if __name__ == "__main__":
-    main()
+    answer = find_societies_by_link(link)
+    print(answer)
+    
